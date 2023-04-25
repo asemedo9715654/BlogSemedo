8d7c4b97e47f7cb385a430520cfa2a18d6cd8c80
Angelo <Semedo>
2023-04-25 15:13:32 -0100

    inicio do projecto

---
+++ Controllers/ArtigosController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting.Internal;
+using System.Xml.Linq;
+using IHostingEnvironment = Microsoft.AspNetCore.Hosting.IHostingEnvironment;
+
+namespace BlogSemedo.Controllers
+{
+    public class ArtigosController : Controller
+    {
+        private readonly IHostingEnvironment _hostingEnvironment;
+
+        public ArtigosController(IHostingEnvironment hostingEnvironment)
+        {
+            _hostingEnvironment = hostingEnvironment;
+        }
+        public IActionResult Index()
+        {
+
+            string webRootPath = _hostingEnvironment.WebRootPath;
+            string xmlFilePath = Path.Combine(webRootPath, "dados", "25-04-2023_Primeiro-Poste.xml");
+
+            return View();
+        }
+    }
+}
